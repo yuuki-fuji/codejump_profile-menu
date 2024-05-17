676cafa012dd5fdb2aff36a6437ac1c1acecaa15
Yuuki-Fujimoto-gtm <yuuki.fujimoto@gtmi.co.jp>
2024-05-17 21:29:26 +0900

    fix header nav font size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
       <h1 className="w-[120px] py-[10px]">
         <img src="/logo.svg" alt="logo" />
       </h1>
-      <nav className="flex  py-[10px]">
+      <nav className="flex py-[10px] text-[0.9rem]">
         <li className="text-secondary ml-[30px]">About</li>
         <li className="text-secondary ml-[30px]">Bicycle</li>
       </nav>
